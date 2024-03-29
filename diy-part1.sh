e28b4f352da64cfc96ba407cbae51c516ee0186c
eearphon <46431124+eearphon@users.noreply.github.com>
2024-03-29 19:03:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,8 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-git checkout dc4d37c71423bd48b02f614414b149fa192d3406
+#Fri Mar 29 19:02:39 CST 2024
+#git checkout dc4d37c71423bd48b02f614414b149fa192d3406
 
 cat >> ./feeds.conf.default <<EOF
 src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main
