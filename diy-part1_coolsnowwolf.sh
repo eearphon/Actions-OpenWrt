4e547d15f0008ae9c90e8b5ba537c78bc1b8815b
eearphon <46431124+eearphon@users.noreply.github.com>
2024-07-23 17:23:37 +0800

    Update diy-part1_coolsnowwolf.sh

--- diy-part1_coolsnowwolf.sh
+++ diy-part1_coolsnowwolf.sh
@@ -20,9 +20,10 @@
 #Fri Mar 29 19:02:39 CST 2024
 #git checkout dc4d37c71423bd48b02f614414b149fa192d3406
 
-cat >> ./feeds.conf.default <<EOF
-src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git
-src-git passwall https://github.com/xiaorouji/openwrt-passwall.git
-src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git
+sed -i '1i\
+src-git packages_up https://github.com/eearphon/packages_up.git\
+src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git\
+src-git passwall https://github.com/xiaorouji/openwrt-passwall.git\
+src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git\
 src-git helloworld https://github.com/fw876/helloworld.git
-EOF
+' ./feeds.conf.default
